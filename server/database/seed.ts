448db62dd62879258d71d8de9d98b6b8d443d448
Maxime Rossier <rossier.maxime@gmail.com>
2026-02-21 14:26:35 +0100

    add db reset

--- server/database/seed.ts
+++ server/database/seed.ts
@@ -1,8 +1,11 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
 import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { reset } from 'drizzle-seed'
+import * as schema from './schema'
 
 async function seed() {
   const db = drizzle(process.env.NUXT_DB!)
+  await reset(db, schema)
 }
 
 seed()
