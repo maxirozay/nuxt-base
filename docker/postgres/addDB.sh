7f8c34bd75115f18c9cca3a4f126ec4f3ea46554
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-02 10:23:40 +0100

    improve addDB

--- docker/postgres/addDB.sh
+++ docker/postgres/addDB.sh
@@ -3,23 +3,23 @@
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 else
-  echo "❌ Error: .env file not found."
+  echo "Error: .env file not found."
   exit 1
 fi
 
 if [ -z "$1" ]; then
-  echo "Usage: ./create-db.sh <database_name>"
+  echo "Usage: ./addDB.sh <database_name>"
   exit 1
 fi
 
 DB_NAME=$1
 
-docker exec -e PGPASSWORD="$PG_PASSWORD" postgres \
-  psql -U "$PG_USER" -d postgres -c "CREATE DATABASE $DB_NAME;"
+docker exec -e PGPASSWORD="$POSTGRES_PASSWORD" postgres \
+  psql -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $DB_NAME;"
 
 if [ $? -eq 0 ]; then
-  echo "✅ Database '$DB_NAME' created successfully!"
-  echo "🔗 Connection String: postgres://$PG_USER:$PG_PASSWORD@localhost:5432/$DB_NAME"
+  echo "Database '$DB_NAME' created successfully!"
+  echo "Connection String: postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$DB_NAME"
 else
-  echo "❌ Failed to create database."
+  echo "Failed to create database."
 fi
