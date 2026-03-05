19cf3e7ccb29f9600da55e4221b14e4958d0c623
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-05 10:57:44 +0100

    remove middleware

--- server/utils/log.ts
+++ server/utils/log.ts
@@ -1,12 +1,12 @@
 import { logs } from '../database/schema'
 
 export async function logEvent(event: any, info?: string, type?: string) {
-  const userId = event.context.user?.id || null
+  const session = await getUserSession(event)
   const ipAddress = event.node.req.headers['x-forwarded-for'] || event.node.req.socket.remoteAddress
   const userAgent = event.node.req.headers['user-agent']
 
   await db.insert(logs).values({
-    userId,
+    userId: session?.user?.id,
     path: event.path,
     method: event.node.req.method,
     ipAddress,
