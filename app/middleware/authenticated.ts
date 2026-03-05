4e59931ce8e748228a02c3c38e2b4f4c0facdd89
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-05 08:57:49 +0100

    remove middleware and add logs

--- app/middleware/authenticated.ts
+++ app/middleware/authenticated.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
   const { loggedIn } = useUserSession()
+  console.log('app middleware')
 
   if (!loggedIn.value) {
     return navigateTo('/signin')
