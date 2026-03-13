e16676f8fd777b289967cbe383d63450fd725559
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-13 16:53:44 +0100

    add language translation

--- layers/base/app/components/LocaleSwitcher.vue
+++ layers/base/app/components/LocaleSwitcher.vue
@@ -6,7 +6,7 @@ const { $getLocales, $switchLocale, getLocale } = useI18n()
   <select
     v-if="$getLocales().length > 1"
     class="locales bg bg-border"
-    aria-label="Language"
+    :aria-label="$t('language')"
     @change="$switchLocale($event.target.value)"
   >
     <option
