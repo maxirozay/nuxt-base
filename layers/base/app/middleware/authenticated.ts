c61d8e8689d99dacfb4b4fc5a4599228a8a0194f
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-16 09:55:30 +0100

    allow sign page on auth middleware

--- layers/base/app/middleware/authenticated.ts
+++ layers/base/app/middleware/authenticated.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession()
 
-  if (!loggedIn.value) {
+  if (!loggedIn.value && to.name !== 'signin') {
     const redirectUrl = '/signin' + (to.fullPath !== '/' ? '?goto=' + to.fullPath : '')
     return navigateTo(redirectUrl, { replace: true })
   }
