7a69f86114931042a3adb9decfd6cf918e594ce1
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-16 10:16:18 +0100

    allow localized sign page in auth middleware

--- layers/base/app/middleware/authenticated.ts
+++ layers/base/app/middleware/authenticated.ts
@@ -1,7 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession()
 
-  if (!loggedIn.value && to.name !== 'signin') {
+  if (!loggedIn.value) {
+    if (String(to.name)?.endsWith('signin')) return
     const redirectUrl = '/signin' + (to.fullPath !== '/' ? '?goto=' + to.fullPath : '')
     return navigateTo(redirectUrl, { replace: true })
   }
