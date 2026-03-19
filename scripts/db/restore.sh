ccab58e549b9c5728811d964e73cb4151eb41923
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-19 15:29:58 +0100

    add db restore script

---
+++ scripts/db/restore.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+source ./scripts/env.sh
+
+BACKUP_PATH="$1"
+if [ -z "$BACKUP_PATH" ]; then
+  echo "Usage: $0 <backup-file>"
+  exit 1
+fi
+
+echo $NUXT_DB
+pg_restore -d "$NUXT_DB" "$BACKUP_PATH"
