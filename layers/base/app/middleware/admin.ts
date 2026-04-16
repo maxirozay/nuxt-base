a9fcd30082530079e49e196012dfa6899a7c2015
Maxime Rossier <rossier.maxime@gmail.com>
2026-04-16 10:20:20 +0200

    don't redirect signed in users to signin

--- layers/base/app/middleware/admin.ts
+++ layers/base/app/middleware/admin.ts
@@ -1,8 +1,14 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { user } = useUserSession()
 
-  if (user.value?.role !== 'admin') {
+  if (!user.value) {
     const redirectUrl = '/signin' + (to.fullPath !== '/' ? '?goto=' + to.fullPath : '')
     return navigateTo(redirectUrl, { replace: true })
+  } else if (user.value.role !== 'admin') {
+    return createError({
+      statusCode: 403,
+      statusMessage: 'Forbidden',
+      message: 'You do not have permission to access this page.',
+    })
   }
 })
