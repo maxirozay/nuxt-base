59252530630582af03947c7e8b8d4f5e2f9ca6f0
Maxime Rossier <dev@maximerossier.ch>
2026-07-14 12:59:43 +0200

    remove unused field

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,4 +1,4 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { relations } from './relations'
 
-export const db = drizzle(useRuntimeConfig().db, { relations, casing: 'snake_case' })
+export const db = drizzle(useRuntimeConfig().db, { relations })
