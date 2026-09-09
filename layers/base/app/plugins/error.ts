44aaf3c4a2208d0614fe01874c97e3e09e19fa67
Maxime Rossier <dev@maximerossier.ch>
2026-09-09 13:30:26 +0200

    when there's an error on the server send the original request to the log

--- layers/base/app/plugins/error.ts
+++ layers/base/app/plugins/error.ts
@@ -1,12 +1,14 @@
 import type { NuxtError } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const requestFetch = useRequestFetch()
+
   nuxtApp.vueApp.config.errorHandler = (err, instance, info) => {
     const status = (err as NuxtError).status
     if (status && status >= 400 && status < 500) return
 
     const componentName = instance?.$options?.__name || ''
-    $fetch('/api/log', {
+    requestFetch('/api/log', {
       method: 'POST',
       body: {
         summary: (err as Error).message + ' ' + info,
